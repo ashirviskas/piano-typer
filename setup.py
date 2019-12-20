fdd4f488fbb35a2641854cd1c364de84eb8280d4
ashirviskas <minematas@gmail.com>
2019-12-20 23:32:34 +0200

    initial structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='piano_typer',
+   version='0.1',
+   description='A module for typing using midi piano keyboard',
+   author='minematas (ashirviskas)',
+   author_email='minematas@gmail.com',
+   packages=['piano_typer']
+)
